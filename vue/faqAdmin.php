a5790b57c7cf03a00ad8d8371a144a308212b3a0
Laure <laure.blais@orange.fr>
2020-01-08 12:14:06 +0100

    reparation

---
+++ vue/faqAdmin.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>
+        FAQ (fr)
+    </title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet"
+          href="public/css/styleMain.css">
+
+</head>
+<body >
+    <?php
+    include ('enTete.php');
+    ?>
+    
+    <h1>Questions fréquentes</h1>
+    
+    
+    
+    
+    <?php
+    include('piedPage.php');
+    ?>
+</body>
+</html>
