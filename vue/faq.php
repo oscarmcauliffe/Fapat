054e7f4048c2d650d1e138e3f7da817ed02dd750
Laure <laure.blais@orange.fr>
2020-01-15 12:20:04 +0100

    faq bdd

--- vue/faq.php
+++ vue/faq.php
@@ -1,3 +1,15 @@
+<?php
+$objPdo = new PDO('mysql:host=localhost;dbname=fapat;charset=utf8','root',''); 
+
+$pdoStat=$objPdo->prepare('SELECT * FROM faq');
+
+$executeIsOk=$pdoStat->execute();
+
+$faqs=$pdoStat->fetchAll();
+
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -19,80 +31,25 @@
     ?>
     <div class ="background">
 
-    <div class="contenu">
-    <h1>Questions fréquentes</h1>
-        <!-- à rép pour chaque question-->
-        <div class="section">
-            <h3>Combien de fois puis-je passer le test ? </h3>
-            <div class="reponse">
-                <div class="reponse-inner">
-                    <p>Vous avez la possibilité de passer le test une fois par candidature. <br/> Cependant vous avez la possibilité de candidater au post de pilote de chasse à deux reprises. <br/> C'est pourquoi nous publions les pourcentages d'admissibilité au métier de pilote de chasse dans la rubrique "statistics". Si vous avez échoué une première fois le test FAPAT, nous vous conseillons vivement de consulter cette page avant de postuler une seconde fois. Ainsi vous aurez une idée plus précise de vos chance de réussite lors de votre deuxième test.</p>
+        <div class="contenu">
+    
+    
+            <h1>Questions fréquentes</h1>  
+             <?php foreach ($faqs as $faq):  ?>
+                <div class="section">
+                   <h3><?= $faq['question'] ?></h3>
+                     <div class="reponse">
+                         <div class="reponse-inner">
+                             <p><?= $faq['reponse'] ?> </p>
+                         </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-        <!-- fin question-->
-        <!-- question 2:  -->
-        <div class="section">
-            <h3>Où puis-je voir mes résultats ?</h3>
-            <div class="reponse">
-                <div class="reponse-inner">
-                    <p>Vous pouvez consulter vos résultats sur votre page Profil. <br/> Vous pouvez également voir votre classement vis-à-vis des autres candidats via la page Statistiques.</p>
-                </div>
-            </div>
-        </div>
-        <!--question 3:  -->
-        <div class="section">
-            <h3>Puis-je télécharger et partager mes résultats ?</h3>
-            <div class="reponse">
-                <div class="reponse-inner">
-                    <p>Le téléchargement et le partage de vos résultats ne sont pas disponibles sur notre site pour des raisons de sécurité.</p>
-                </div>
-            </div>
-        </div>
-        <!-- question 4: -->
-        <div class="section">
-            <h3>Où puis-je consulter les informations relatives au test et à l'armée de l'air ?</h3>
-            <div class="reponse">
-                <div class="reponse-inner">
-                    <p>Les informations sur l'armée de l'air et sur le test sont disponibles dans l'onglet Documentation.<br/> Vous pouvez retrouver cet onglet dans le Menu Principal et dans l'en tête. </p>
-                </div>
-            </div>
+            <?php endforeach ?>
         </div>
 
-        <!-- -->
-
-        <div class="section">
-            <h3>Questions 2:</h3>
-            <div class="reponse">
-                <div class="reponse-inner">
-                    <p>Nec sane haec sola pernicies orientem diversis cladibus adfligebat. Namque et Isauri, quibus est usitatum saepe pacari saepeque inopinis excursibus cuncta miscere, ex latrociniis occultis et raris, alente inpunitate adulescentem in peius audaciam ad bella gravia proruperunt, diu quidem perduelles spiritus inrequietis motibus erigentes, hac tamen indignitate perciti vehementer, ut iactitabant, quod eorum capiti quidam consortes apud Iconium Pisidiae oppidum in amphitheatrali spectaculo feris praedatricibus obiecti sunt praeter morem.</p>
-                </div>
-            </div>
-        </div>
-      <!-- -->
-
-        <div class="section">
-            <h3>Qui a fait ce merveilleux site ? Je le trouve super beau et aimerait en savoir plus sur ses créateurs.</h3>
-            <div class="reponse">
-                <div class="reponse-inner">
-                    <p>Merci du compliment. Cet extraordinaire site a été réalisé par l'entreprise CapSens. <br/> Vous pouvez retrouver plus d'informations dans l'onglet "<a href="index.php?action=quiSommesNous">Qui Sommes-nous ? <a/>" .</p>
-                </div>
-            </div>
-        </div>
-
-        <div class="section">
-            <h3>I want this page to be in English</h3>
-            <div class="reponse">
-                <div class="reponse-inner">
-                    <p>You will be able to change the language of the site thanks to the button on top-right of the page. <br/> Two languages are available or now: French and English.</p>
-                </div>
-            </div>
-        </div>
     </div>
-
-  </div>
     <?php
     include('piedPage.php');
     ?>
 </body>
-</html>
+</html> 
